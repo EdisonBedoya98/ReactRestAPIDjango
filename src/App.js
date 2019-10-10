cffc53356785062c6e10a90be7c5a62d8c309942
EdisonBedoya98 <edison.bedoyag@udea.edu.co>
2019-10-09 19:50:59 -0500

    caracteristicas nuevas de la GUI

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
         render={data => <Table data={data} />} /> 
                       api/mlpindex/
         */}
-        <FormSentimentIndex/>
+        <FormSentimentIndex />
       </div>
 
       <div>
@@ -32,11 +32,41 @@ function App() {
             <Card.Title>Información sobre la APP</Card.Title>
             <Card.Subtitle className="mb-2 text-muted">MIDANSE</Card.Subtitle>
             <Card.Text>
-              La aplicación clasifica una opinión como positiva o negativa,
-                dada por el usuario que puede ser sobre hoteles o sobre Iphone, un ejemplo
-                de una opinión negativa puede ser: Me intentaron estafar un poco con una Suite y el wifi no llegaba a la habitación que nos dieron, y un ejemplo de
-                 una opinión positiva puede ser: Todo muy bien, ubicación, limpieza, decoración, tamaño de la habitación, albornoz.
-             </Card.Text>            
+
+
+              La aplicación MIDANSE clasifica una opinión como positiva o negativa. Actualmente funciona bajo dos modelos entrenados con textos de dos dominios: revisiones de servicios hoteleros y tweets sobre el producto Iphone. Ejemplos de opiniones que puede clasificar la aplicación:
+              
+ </Card.Text>
+            <Card.Text>
+              "Me intentaron estafar un poco con una Suite y el wifi no llegaba a la habitación que nos dieron" <b>(Negativa)</b>
+             
+             
+   </Card.Text>
+            <Card.Text>
+              "Todo muy bien, ubicación, limpieza, decoración, tamaño de la habitación, albornoz" <b>(Positiva)</b>
+           
+           
+     </Card.Text>
+            <Card.Body>
+              <Card.Text>Desarrollada por:</Card.Text>
+              <Card.Text>Edison Bedoya (edison.bedoyag@udea.edu.co)</Card.Text>
+              <Card.Text>Santiago Quintero (santiago.quinteroh@udea.edu.co )</Card.Text>
+              <Card.Text>Antonio Tamayo (ajtamayoh@gmail.com)</Card.Text>
+            </Card.Body>
+            <Card.Text></Card.Text>
+            <Card.Text>
+
+            <Card.Text> Departamento de Ingeniería de Sistemas</Card.Text>
+
+            <Card.Text> Facultad de Ingeniería</Card.Text>
+            <Card.Text>Universidad de Antioquia</Card.Text>
+             
+             <Card.Text>Medellín-Colombia</Card.Text>
+              <Card.Text>   2019</Card.Text>
+              
+           
+              
+</Card.Text>
           </Card.Body>
         </Card>
 
