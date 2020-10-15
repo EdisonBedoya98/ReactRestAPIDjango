c87034975b10ecdfbfe64689b74962ad4def2785
edison <edison.bedoyag@udea.edu.co>
2020-10-14 21:19:15 -0500

    update

--- src/FormSentimentIndex.js
+++ src/FormSentimentIndex.js
@@ -1,27 +1,25 @@
 import React, { Component } from "react";
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import Nav from 'react-bootstrap/Nav'
 import ButtonCarga from "./ButtonCarga";
 
 class FormSentimentIndex extends Component {
     state = {
         loaded: false,
         clearInput: false,
-        texto: "",
+        text: "",
         feeling: "",
-        endpoint: "https://apidjangoproyectoingtegrador1.herokuapp.com/api/mlpindex/"
+        endpoint: "http://127.0.0.1:8000/model_analysis/"
     };
     handleChange = e => {
         this.setState({ [e.target.name]: e.target.value });
     };
     handleSubmit = e => {
         e.preventDefault();
-        const { texto} = this.state;
+        const { text} = this.state;
         this.setState({clearInput:!this.state.clearInput})
-        if(texto!==""&& this.state.clearInput===false){
-            const lead = { texto };
+        if(text!==""&& this.state.clearInput===false){
+            const lead = { text };
             const conf = {
                 method: "post", 
                 body: JSON.stringify(lead),
@@ -30,30 +28,25 @@ class FormSentimentIndex extends Component {
             fetch(this.state.endpoint, conf).then(response => {
                 return response.json();
             }).then(data => {
-                this.setState({ feeling: data, loaded: true })
-                this.render()
+                console.log(data)
+                this.setState({ feeling: data.feeling, loaded: true })
+              //  this.setState({ feeling: data, loaded: true })
+               this.render()
     
             });
 
         }
         if(this.state.clearInput===true)
         {
-            this.setState({ texto: "",feeling:""})
+            this.setState({ text: "",feeling:""})
             this.render()
         }
      
     };
-    handleSelect = eventKey => {
-        if (eventKey === "RevHotel") {
-            this.setState({ endpoint: "https://apidjangoproyectoingtegrador1.herokuapp.com/api/mlpindex/" })
-        } else {
-            this.setState({ endpoint: "https://apidjangoproyectoingtegrador1.herokuapp.com/api/index/" })
-        }
 
-
-    };
     render() {
-        const { loaded, texto, feeling } = this.state;
+        const { loaded, text, feeling } = this.state;
+        console.log(feeling)
         return (
             <div>
                 <div className="column">
@@ -67,9 +60,9 @@ class FormSentimentIndex extends Component {
                                             autoFocus
                                             className="input"
                                             type="text"
-                                            name="texto"
+                                            name="text"
                                             onChange={this.handleChange}
-                                            value={texto}
+                                            value={text}
                                             required
                                             size="100" />
                                     </div>
@@ -78,28 +71,14 @@ class FormSentimentIndex extends Component {
                                     <div className="mt-5">
                                         El sentimiento para este texto es <b>{loaded ? (() => {
                                             switch (feeling) {
-                                                case "[[0]]": return "Negativo";
-                                                case "[[1]]": return "Positivo";
+                                                case 0: return "Negativo";
+                                                case 1: return "Positivo";
                                                 default: return "Buscando";
                                             }
                                         })()
                                             : 'Esperando análisis'}</b>
                                     </div>
-                                </Row>
-                                {/* <Row className="justify-content-md-center">
-                                    <Col md="auto" >
-                                        <div className="mt-4">
-                                            <Nav variant="pills" defaultActiveKey="RevHotel" onSelect={this.handleSelect}>
-                                                <Nav.Item>
-                                                    <Nav.Link eventKey="RevHotel">Revisión Hotel</Nav.Link>
-                                                </Nav.Item>
-                                                <Nav.Item>
-                                                    <Nav.Link eventKey="RevIphone">Tweet Iphone</Nav.Link>
-                                                </Nav.Item>
-                                            </Nav>
-                                        </div>
-                                    </Col>
-                                </Row> */}
+                                </Row>                              
                             </Container>
                         </div>
                         <div className="control mt-5">                          
