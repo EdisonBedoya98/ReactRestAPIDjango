a4a0b0db729880ef6b1fcd1096051f89f6b1165b
edison <edison.bedoyag@udea.edu.co>
2021-01-31 21:25:57 -0500

    Se sube con archivos compilados

---
+++ build/precache-manifest.879f9a7f288ae5981ec83816edfbd749.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18d1763602b50bda267fc28f72a4eab2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55287e162a9169915648",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "55287e162a9169915648",
+    "url": "/static/js/2.6b6132eb.chunk.js"
+  },
+  {
+    "revision": "c12a8782844eb2fbe526",
+    "url": "/static/js/main.896eac30.chunk.js"
+  },
+  {
+    "revision": "3a2332c506774a052968",
+    "url": "/static/js/runtime-main.39d25160.js"
+  }
+]);
